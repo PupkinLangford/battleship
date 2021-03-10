9db3fc74c61d98ec0bab4b163ced8cae926c34ab
PupkinLangford <tahellman@gmail.com>
2021-03-10 18:51:26 -0500

    Updates

---
+++ precache-manifest.ed8bdfe13229aed4689e7613ae3da7c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8de1f76ac5fc8a847efb40c449f0c81",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "75082e43ce3c57c0d55c",
+    "url": "/battleship/static/css/main.a8a24b77.chunk.css"
+  },
+  {
+    "revision": "ae74b6deb6420af25832",
+    "url": "/battleship/static/js/2.83df0ab2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/battleship/static/js/2.83df0ab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75082e43ce3c57c0d55c",
+    "url": "/battleship/static/js/main.3a912aee.chunk.js"
+  },
+  {
+    "revision": "1576b120f8ea920fbd40",
+    "url": "/battleship/static/js/runtime-main.9e463cc9.js"
+  }
+]);
